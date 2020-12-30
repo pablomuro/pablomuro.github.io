a053413d5d28dc84778d4d75421ea682305157e1
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2020-12-30 20:10:09 -0300

    add index for blog folder

---
+++ pages/blog/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="m-8">
+    <!-- <TheHeader /> -->
+
+    <h1 class="">Blog Posts</h1>
+    <ul class="flex flex-wrap">
+      <li v-for="article of articles" :key="article.slug" class="a"></li>
+    </ul>
+
+    <footer class="flex justify-center border-gray-500 border-t-2">
+      <p class="mt-4">
+        Created by
+        <a
+          href="https://twitter.com/debs_obrien"
+          class="font-bold hover:underline"
+          >Debbie O'Brien</a
+        >
+        at NuxtJS. See the
+        <a
+          href="https://nuxtjs.org/blog/creating-blog-with-nuxt-content"
+          class="font-bold hover:underline"
+          >tutorial</a
+        >
+        for how to build it.
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, app }) {
+    const articles = await $content(app.i18n.locale)
+      .only(['title', 'description', 'img', 'slug', 'author'])
+      .sortBy('createdAt', 'desc')
+      .fetch()
+    return {
+      articles,
+    }
+  },
+}
+</script>
+
+<style class="postcss">
+.article-card {
+  border-radius: 8px;
+}
+.article-card a {
+  background-color: #fff;
+  border-radius: 8px;
+}
+.article-card img div {
+  border-radius: 8px 0 0 8px;
+}
+</style>
