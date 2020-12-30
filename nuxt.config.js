218f2a778114b4edcaed8f56212dd065c2c6a0d7
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2020-12-30 20:09:36 -0300

    fix favicon path

--- nuxt.config.js
+++ nuxt.config.js
@@ -95,7 +95,7 @@ export default {
       },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
       {
         rel: 'apple-touch-icon',
         type: 'image/x-icon',
