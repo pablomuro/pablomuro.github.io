af4f040f12451719c35a3f6b8340b8b2434fa726
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2021-01-02 08:05:23 -0300

    add author meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,6 +109,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'author',
+        name: 'author',
+        content: 'PAblo A. Muro Martinez',
+      },
       ...getHeadMetaTags(),
     ],
     link: [...getHeadFavicons(), { rel: 'manifest', href: '/manifest.json' }],
