0cf6062b1b27fc3ed61ca243727cae81a0532db1
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2021-02-18 01:54:36 -0300

    feature/separete custom select layput

--- components/BlogSearchInput.vue
+++ components/BlogSearchInput.vue
@@ -6,7 +6,7 @@
         type="search"
         autocomplete="off"
         placeholder="Search Posts"
-        class="cursor-pointer relative w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition-all ease-in-out duration-150 sm:text-sm sm:leading-5"
+        class="custom-select"
       />
     </span>
 
@@ -26,7 +26,7 @@
           :key="post.slug"
           tabindex="0"
           role="option"
-          class="text-gray-900 select-none relative py-2 pl-3 pr-9 cursor-pointer hover:text-white hover:bg-indigo-600 focus:outline-none focus:text-white focus:bg-indigo-600"
+          class="custom-select-li"
         >
           <div class="flex items-center space-x-3">
             <NuxtLink
