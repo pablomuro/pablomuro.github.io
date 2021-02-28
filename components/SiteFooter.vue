6d993572352a1fb176ac3ddb62c9edc9466cea6f
pablomuro <pablo.muro@gmail.com>
2021-02-28 00:28:39 -0300

    refactory layout

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,7 +1,7 @@
 <template>
-  <footer class="relative mt-32 bg-theme-100">
+  <footer class="relative mt-24 sm:mt-32 bg-theme-100">
     <div
-      class="created-by bg-theme-600 relative -mt-8 p-5 w-1/3 mx-auto text-center rounded-md"
+      class="created-by bg-theme-600 relative -mt-4 sm:-mt-8 mx-auto text-center rounded-md p-2 sm:p-5 w-4/5 sm:w-1/3 text-xs sm:text-inherit"
     >
       <code class="language-html"
         ><span class="token tag"
@@ -18,42 +18,38 @@
         >
       </code>
     </div>
-    <div class="flex items-center justify-center mt-2">
-      <div class="container flex flex-col items-center mt-3 space-y-3">
-        <div class="social space-x-2 text-black">
-          <a :href="emailLink" target="_blank" rel="noopener noreferrer">
-            <fa :icon="['fas', 'envelope']" />
-          </a>
-          <a :href="githubLink" target="_blank" rel="noopener noreferrer">
-            <fa :icon="['fab', 'github']" />
-          </a>
-          <a :href="linkedinLink" target="_blank" rel="noopener noreferrer">
-            <fa :icon="['fab', 'linkedin']" />
-          </a>
-          <a :href="instagramLink" target="_blank" rel="noopener noreferrer">
-            <fa :icon="['fab', 'instagram']" />
-          </a>
-          <a :href="twitterLink" target="_blank" rel="noopener noreferrer">
-            <fa :icon="['fab', 'twitter']" />
-          </a>
-          <a :href="resumeLink" target="_blank" rel="noopener noreferrer">
-            <fa :icon="['fas', 'file']" />
-          </a>
-        </div>
-        <div class="my-2">Copyright © {{ year }} {{ brand }}</div>
+
+    <div class="flex flex-col items-center justify-center mt-2">
+      <div class="social space-x-2 text-black mt-2 sm:mt-4">
+        <a :href="emailLink" target="_blank" rel="noopener noreferrer">
+          <fa :icon="['fas', 'envelope']" />
+        </a>
+        <a :href="githubLink" target="_blank" rel="noopener noreferrer">
+          <fa :icon="['fab', 'github']" />
+        </a>
+        <a :href="linkedinLink" target="_blank" rel="noopener noreferrer">
+          <fa :icon="['fab', 'linkedin']" />
+        </a>
+        <a :href="instagramLink" target="_blank" rel="noopener noreferrer">
+          <fa :icon="['fab', 'instagram']" />
+        </a>
+        <a :href="twitterLink" target="_blank" rel="noopener noreferrer">
+          <fa :icon="['fab', 'twitter']" />
+        </a>
+        <a :href="resumeLink" target="_blank" rel="noopener noreferrer">
+          <fa :icon="['fas', 'file']" />
+        </a>
       </div>
+      <div class="my-2 sm:mt-4">Copyright © {{ year }} {{ brand }}</div>
     </div>
   </footer>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-// @ts-ignore
-import NuxtJs from '~/assets/icons/nuxt-js.svg?inline'
 
 export default Vue.extend({
   name: 'SiteFooter',
-  components: { NuxtJs },
   data() {
     return {
       name: this.$config.myData.fullName,
@@ -76,7 +72,7 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 footer {
-  height: 150px;
+  height: 100px;
 
   code {
     background-color: inherit;
@@ -84,8 +80,8 @@ footer {
 
   .social {
     svg {
-      width: 1.25rem;
-      height: 1.25rem;
+      @apply w-4 h-4;
+
       path {
         @apply fill-current text-theme-600;
       }
@@ -93,6 +89,17 @@ footer {
 
     svg:hover path {
       @apply fill-current text-theme-400;
+    }
+  }
+}
+
+@screen sm {
+  footer {
+    height: 150px;
+    .social {
+      svg {
+        @apply w-5 h-5;
+      }
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,30 @@
 <template>
   <page>
     <template #title>Home</template>
-    <div class="name">Pablo Muro Dev - {{ year }}</div>
+    <div class="flex flex-col items-center justify-center">
+      <section
+        class="section-hi flex flex-col sm:flex-row items-center my-auto px-4 sm:px-0"
+      >
+        <div class="relative flex -ml-8 sm:mr-20">
+          <div
+            class="block avatar"
+            :style="{
+              backgroundImage: `url(${require('@/assets/images/avatar.png')})`,
+            }"
+          ></div>
+          <div class="hi-box absolute">
+            <fa
+              :icon="['far', 'comment']"
+              class="absolute fill-current text-theme-500 hi-bubble text-6xl sm:text-7xl"
+            />
+            <h1 class="absolute hi hi-text text-xl sm:text-3xl">Hi!</h1>
+          </div>
+        </div>
+        <div class="text-center text-lg">
+          <h1>My name is Pablo and I'm a Fullstack Developer</h1>
+        </div>
+      </section>
+    </div>
   </page>
 </template>
 
@@ -22,8 +45,49 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.name {
-  text-align: center;
-  margin-top: 20vh;
+.section-hi {
+  margin-top: 8vh;
+  .hi-box {
+    left: 4.9rem;
+    top: 1.5rem;
+
+    .hi-bubble {
+      transform: rotate(8deg);
+    }
+
+    .hi-text {
+      left: 1rem;
+      margin: 0.9rem 0 0 0;
+    }
+  }
+
+  .avatar {
+    background-color: #fff;
+    background-position: bottom;
+    background-repeat: no-repeat;
+    background-size: auto 90%;
+
+    height: 100px;
+    width: 100px;
+  }
+}
+
+@screen sm {
+  .section-hi {
+    margin-top: 20vh;
+    .avatar {
+      height: 130px;
+      width: 130px;
+    }
+    .hi-box {
+      left: 6.5rem;
+
+      .hi-bubble {
+      }
+
+      .hi-text {
+      }
+    }
+  }
 }
 </style>
