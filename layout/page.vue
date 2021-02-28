24ddd370dfe1960550ad903b5017b65caaa775cb
pablomuro <pablo.muro@gmail.com>
2021-02-28 00:44:56 -0300

    change css place

--- layout/page.vue
+++ layout/page.vue
@@ -1,9 +1,9 @@
 <template>
-  <section>
+  <section class="">
     <page-title>
       <slot name="title"></slot>
     </page-title>
-    <div class="page-container relative z-10">
+    <div class="page-container relative z-10 flex-grow">
       <slot></slot>
     </div>
   </section>
@@ -15,3 +15,11 @@ export default Vue.extend({
   name: 'Page',
 })
 </script>
+<style lang="scss" scoped>
+.page-container {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 960px;
+  height: 100%;
+}
+</style>
