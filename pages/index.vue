2facbe8f9b1fce5a650c520208bb97d6df88d0f8
pablomuro <pablo.muro@gmail.com>
2021-03-01 09:36:45 -0300

    add index content

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <page page-title="Home">
     <div class="flex flex-col items-center justify-center">
       <section
-        class="section-hi flex flex-col sm:flex-row items-center my-auto px-4 sm:px-0"
+        class="section-hi flex flex-col sm:flex-row items-center my-auto px-4 sm:px-0 mt-14"
       >
         <div class="relative flex -ml-8 sm:mr-20">
           <div
@@ -23,15 +23,38 @@
           <h1>My name is Pablo and I'm a Fullstack Developer</h1>
         </div>
       </section>
+      <div class="mt-14">
+        <pre
+          class="language-js"
+        ><code><span class="token keyword">const</span> <span class="token function-variable function">developBlog</span> <span class="token operator">=</span> <span class="token keyword">async</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token arrow operator">=&gt;</span> <span class="token punctuation">{</span>
+  <span class="token keyword control-flow">await</span> <span class="token keyword">new</span> <span class="token class-name">Blog</span><span class="token punctuation">(</span><span class="token punctuation">{</span><fa icon="heart" :style="{ color: 'red' }" /><span class="token punctuation"
+    >, </span><fa :icon="['fab', 'js-square']" :style="{ color: '#F7DF1E' }" /><span
+    class="token punctuation"
+    >, </span><fa :icon="['fab', 'node']" :style="{ color: '#339933' }" /><span
+    class="token punctuation"
+    >, </span
+  ><fa :icon="['fab', 'vuejs']" :style="{ color: '#4FC08D' }" /><span
+    class="token punctuation"
+    >, </span><nuxt-js
+    class="svg-inline--fa"
+    :style="{ fill: '#00C58E', width: '1rem', height: '1rem' }"
+  /><span class="token punctuation">}</span><span class="token punctuation">)</span>
+<span class="token punctuation">}</span>
+</code></pre>
+      </div>
     </div>
   </page>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+// @ts-ignore
+import NuxtJs from '~/assets/icons/nuxt-js.svg?inline'
+
 import { getHtmlHead } from '~/utils/headUtils'
 
 export default Vue.extend({
+  components: { NuxtJs },
   head() {
     return { ...getHtmlHead.call(this) }
   },
