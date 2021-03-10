55ee1ac47111a1bca4843ca05fe684a813e1a9b1
pablomuro <pablo.muro@gmail.com>
2021-03-09 21:55:19 -0300

    fix: new size for logo

--- components/LogoWrapper.vue
+++ components/LogoWrapper.vue
@@ -51,10 +51,10 @@ export default Vue.extend({
       return extension
     },
     imgWebp(): Img {
-      return require(`@/assets/images/${this.src}?resize&sizes[]=90&format=webp`) as Img
+      return require(`@/assets/images/${this.src}?resize&sizes[]=92&format=webp`) as Img
     },
     defaultImg(): Img {
-      return require(`@/assets/images/${this.src}?resize&sizes[]=90`) as Img
+      return require(`@/assets/images/${this.src}?resize&sizes[]=92`) as Img
     },
   },
 })
