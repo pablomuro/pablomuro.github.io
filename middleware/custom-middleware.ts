dd4e7211a2646c0702cec1c1507c06fbd7677c43
pablomuro <pablo.muro@gmail.com>
2021-03-28 20:04:05 -0300

    fix: language server side guard

--- middleware/custom-middleware.ts
+++ middleware/custom-middleware.ts
@@ -1,18 +1,29 @@
 import { Middleware } from '@nuxt/types'
 
-const customMiddleware: Middleware = async ({ app: { $i18nGuard, store }, from, route }) => {
-  // if (process.server) return
+const customMiddleware: Middleware = async (context) => {
+  const { app: { $i18nGuard, store }, from, route } = context
 
   if ($i18nGuard.getLocale() === '') {
     await $i18nGuard.setLocale()
   }
 
-  if (from && from.name == route.name) {
-
-    await $i18nGuard.guard()
-  }
-
   store?.dispatch('pages/clearTitle')
+
+
+  if (process.server) {
+    if (await $i18nGuard.guard()) {
+      const from = context.i18n.locale
+      const to = $i18nGuard.getLocale()
+
+      return context.redirect({ name: route.name?.replace(from, to) })
+    }
+  } else {
+
+    if (from && from.name == route.name) {
+
+      await $i18nGuard.guard()
+    }
+  }
 
 }
 
