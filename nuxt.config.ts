c0758a0e69d11b1db74fb2219ef5089386d38514
pablomuro <pablo.muro@gmail.com>
2021-03-28 21:33:56 -0300

    fix CreatedAt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,6 +74,7 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/content
+    'nuxt-content-git',
     '@nuxt/content',
     // https://i18n.nuxtjs.org/
     'nuxt-i18n',
