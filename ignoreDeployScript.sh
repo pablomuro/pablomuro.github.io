7d1b274b8b2796c253e889e6a4ebdb9fd7e8612c
pablomuro <pablo.muro@gmail.com>
2021-06-21 18:18:18 -0300

    fix/ignoreDeployScript

--- ignoreDeployScript.sh
+++ ignoreDeployScript.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B | cat)
-
-echo "Last commit message:|$LAST_COMMIT_MESSAGE|"
-echo "vercel comit:|$VERCEL_GIT_COMMIT_MESSAGE|"
+echo "Commit message:|$VERCEL_GIT_COMMIT_MESSAGE|"
 echo "Branch: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$LAST_COMMIT_MESSAGE" == *"[pablomuro-blog-manager]"* && ("$VERCEL_GIT_COMMIT_REF" == "development" || "$VERCEL_GIT_COMMIT_REF" == "main") ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_MESSAGE" == *"[pablomuro-blog-manager]"* && ("$VERCEL_GIT_COMMIT_REF" == "development" || "$VERCEL_GIT_COMMIT_REF" == "main") ]] ; then
   echo "🛑 - Build cancelled"
   exit 0;
 else
