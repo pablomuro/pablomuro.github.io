5d30e3709f990ca2200a384486aae26654a1b384
pablomuro <pablo.muro@gmail.com>
2021-06-20 22:56:30 -0300

    feature/add ignore script

---
+++ ignoreDeployScript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LAST_COMMIT_MESSAGE=git log -1 --pretty=%B | cat
+
+if [[ "$LAST_COMMIT_MESSAGE" != "[pablomuro-blog-manager] - Blog new Post" ]] ; then
+  echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
