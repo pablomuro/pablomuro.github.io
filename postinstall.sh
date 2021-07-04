0b2b504a30a716f1ef4fd71b7aff4a9d302e1926
pablomuro <pablo.muro@gmail.com>
2021-07-04 10:12:54 -0300

    fix/postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "$PWD"
 if [[ "$VERCEL" == 1 ]] ; then
-  npx sass-migrator division "$PWD"/node_modules/flag-icon-css/sass/flag-icon.scss
-  npx sass-migrator division "$PWD"/node_modules/flag-icon-css/sass/_variables.scss
-  npx sass-migrator division "$PWD"/node_modules/flag-icon-css/sass/_flag-icon-list.scss
-  npx sass-migrator division "$PWD"/node_modules/flag-icon-css/sass/_flag-icon-base.scss
+  npx sass-migrator division "$PWD"/node_modules_dev/flag-icon-css/sass/*.scss
 else
   npx --yes sass-migrator division node_modules/flag-icon-css/sass/*.scss
 fi
