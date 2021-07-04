2cbc2b5d0d1c50968e6f43742935d5b560b7428b
pablomuro <pablo.muro@gmail.com>
2021-07-04 10:06:18 -0300

    test/postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "$(ls $PWD/node_modules/flag-icon-css/sass/)"
+echo "$PWD"
+echo "$(cat $PWD/node_modules/flag-icon-css/sass/flag-icon.scss)"
 
 if [[ "$VERCEL" == 1 ]] ; then
   npm install -g sass-migrator
-  sass-migrator division "$PWD"/node_modules/flag-icon-css/sass/*.scss
+  sass-migrator division "$PWD"/node_modules/flag-icon-css/sass/flag-icon.scss
 else
   npx --yes sass-migrator division node_modules/flag-icon-css/sass/*.scss
 fi
